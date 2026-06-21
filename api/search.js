d5843cf55b8a1f461849a2cd8a5f468bd29f7f6b
Alex Dance <64453972+alexdance2468@users.noreply.github.com>
2026-06-21 17:51:02 +1000

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,3 +1,12 @@
-export default function handler(req, res) {
-  res.json({ status: "ok" });
-}
+create or replace function run_query(query text)
+returns json
+language plpgsql
+security definer
+as $$
+declare
+  result json;
+begin
+  execute 'select json_agg(t) from (' || query || ') t' into result;
+  return result;
+end;
+$$;
